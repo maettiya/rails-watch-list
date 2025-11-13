a3582f956739be80a764db6d09df4bd5e78ca605
Matt McLuckie <mcluckiematt@gmail.com>
2025-11-13 22:40:10 +0900

    Added style to app

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "lists#index"
+
+
+  # LISTS ROUTES
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new", as: "new_list"
+  # post "lists", to: "lists#create"
+  # get "lists/:id", to: "lists#show", as: "list"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
