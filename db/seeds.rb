f73272f973438679417298e005a2405fdfc7886b
Matt McLuckie <mcluckiematt@gmail.com>
2025-11-13 20:41:00 +0900

    Created movie seeds with API

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "json"
+require "open-uri"
+
+puts "Cleaning the database..."
+Movie.destroy_all
+
+puts "Getting movies from TMDB..."
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts "Creating movies..."
+base_poster_url = "https://image.tmdb.org/t/p/original"
+
+movies["results"].each do |movie_data|
+  Movie.create!(
+    title: movie_data["title"],
+    overview: movie_data["overview"],
+    rating: movie_data["vote_average"],
+    poster_url: "#{base_poster_url}#{movie_data["poster_path"]}"
+  )
+  puts "Created #{movie_data["title"]}"
+end
+
+puts "Finished! Created #{Movie.count} movies"
